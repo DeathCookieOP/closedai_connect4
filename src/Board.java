ae67d75e00ac0dec0a992bdece2ba2cc56ca3c36
DeathCookieOP <procookieop@gmail.com>
2024-06-11 19:35:59 -0700

    added beginning documentations

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,20 @@
+/**
+ * INFO ABOUT CLASS
+ *
+ * Date Created: 06/11/2024
+ * Date Last Updated: 06/11/2024
+ * */
+
 public class Board {
+
+
+    /**
+     * Converts the table to a viewable form
+     *
+     * @return Table in console
+     * */
+    public String toString() {
+        return "test";
+    }
 
 }
--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,10 @@
+/**
+ * INFO ABOUT CLASS
+ *
+ * Date Created: 06/11/2024
+ * Date Last Updated: 06/11/2024
+ * */
+
 public class Square {
 
 }
